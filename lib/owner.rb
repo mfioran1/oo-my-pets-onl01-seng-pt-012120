1c5acbfdcc2e6aded3032da382fc380ecbf7e286
Maurice Fiorani <mauricef97@gmail.com>
2020-02-19 18:23:16 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,11 @@
 class Owner
-attr_accessor :name, :pets 
-attr_reader :species
+attr_accessor :pets 
+attr_reader :name, :species
 @@all = []
 
   def initialize(species)
-    @species = species
+    @name = name
+    @species = human
     @@all << self
   end  
 end
